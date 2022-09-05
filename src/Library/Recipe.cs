8988c0155aab25ad2e7291276020ffc933f645aa
IgnacioBerra <ignacio.berra@correo.ucu.edu.uy>
2022-09-04 22:22:18 -0300

    Ejercicio Full GRASP and SOLID, parte 1 RESUELTA.

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -14,6 +14,9 @@ namespace Full_GRASP_And_SOLID.Library
         private ArrayList steps = new ArrayList();
 
         public Product FinalProduct { get; set; }
+        public double InsumosCost {get;set;} = 0;
+        public double EquipmentCost {get;set;}
+        public double TotalCost{get;set;}
 
         public void AddStep(Step step)
         {
@@ -25,6 +28,25 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        // Cumpliendo con el principio Expert, debemos asignarle la responsabilidad de conocer el costo total a la clase que conozca la información necesaria.
+        // La clase Step conoce la información de product y equipment, pero como la clase Step colabora con Recipe, consideré que
+        // la mejor opción era en esta clase. Para esta primer entrega, cree la funcion GetProductionCost(), e imprime el total en la función PrintRecipe().
+        public double GetProductionCost()
+        {
+            foreach (Step Object in steps)
+            {
+              InsumosCost = InsumosCost + (Object.Input.UnitCost * Object.Quantity) ;
+            }
+
+            foreach (Step Object in steps)
+            {
+            EquipmentCost = EquipmentCost + (Object.Equipment.HourlyCost * Object.Time); 
+            }
+
+            TotalCost = InsumosCost + EquipmentCost;
+
+            return TotalCost;
+        }
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
@@ -32,7 +54,9 @@ namespace Full_GRASP_And_SOLID.Library
             {
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
+
             }
+            Console.WriteLine ($"El costo total de la receta es {GetProductionCost()}");
         }
     }
 }
